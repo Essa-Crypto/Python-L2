c9b04d2d2bea1160489a155df9c4b9ffd0191ecb
Essa-Crypto <engr-adnan@live.com>
2025-01-06 20:53:30 +1100

    jzncx nvs z

---
+++ b-days.py
@@ -0,0 +1,16 @@
+print(" Birthdays of 5 friends\n")
+
+a="13/02/0192"
+print("Essa1 -",a)
+
+b="16/05/9995"
+print("\nsunny -",b)
+
+c= "06/07/0029"
+print("\millenial -", c)
+
+d= "06/08/83272"
+print("\idk -", d)
+
+c= "03/09/2013"
+print("\:] -", c)
